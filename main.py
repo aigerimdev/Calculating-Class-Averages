341851f16052140e5beb715677da9b99aee9b0f6
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-03-18 10:50:35 -0700

    implamentation from class activity

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 def calculate_class_averages(classrooms):
-    pass
+    class_averages = {}
+    for classroom, students in classrooms.items():
+        if not students:
+            class_averages[classroom] = 0
+            continue
+        scores = 0
+        num_grades = 0
+        for grades in students.values():
+            for grade in grades:
+                scores += grade
+                num_grades += 1
+        avg = scores / num_grades
+        class_averages[classroom] = avg
+        
+    return class_averages  
+         
+# create result dict
+# for loop throuh classriom to get each class and its student
+# create veraiabke to store all scores
+# loop through students abd get eac grade abd add tgem t the sum variabke
+# find average by deviding sum by number of scires
+# add key value pair into reslut dict(class: avg_score)
+#return result
